9399263383f639d53b9c4e83032d2589cb3279bf
Abdullah Al-Banna <abdu.albanna@proton.me>
2026-02-14 22:10:27 +0300

    get the bulk endpoints

--- src/usb.rs
+++ src/usb.rs
@@ -1,4 +1,8 @@
-use nusb::descriptors::InterfaceDescriptor;
+use nusb::{
+    Endpoint,
+    descriptors::InterfaceDescriptor,
+    transfer::{Bulk, Direction, In, Out},
+};
 
 pub const APPLE_VID: u16 = 0x5ac;
 
@@ -45,4 +49,40 @@ pub async fn get_usbmux_interface<'a>(dev: &'a nusb::Device) -> InterfaceDescrip
     }
 
     intf
+}
+
+pub async fn get_usb_endpoints<'a>(
+    dev: &'a nusb::Device,
+    interface_descriptor: &InterfaceDescriptor<'a>,
+) -> (Endpoint<Bulk, Out>, Endpoint<Bulk, In>) {
+    let mut endpoint_out: Option<Endpoint<Bulk, Out>> = None;
+    let mut endpoint_in: Option<Endpoint<Bulk, In>> = None;
+
+    let intf = dev
+        .claim_interface(interface_descriptor.interface_number())
+        .await
+        .expect("unable to claim the interface");
+
+    for endpoint in interface_descriptor.endpoints() {
+        match endpoint.direction() {
+            Direction::Out => {
+                endpoint_out = Some(
+                    intf.endpoint(endpoint.address())
+                        .expect("unable to get the bulk out endpoint"),
+                );
+            }
+
+            Direction::In => {
+                endpoint_in = Some(
+                    intf.endpoint(endpoint.address())
+                        .expect("unable to get the bulk in endpoint"),
+                );
+            }
+        }
+    }
+
+    (
+        endpoint_out.expect("there was no bulk out endpoint"),
+        endpoint_in.expect("there was no bulk in endpoint"),
+    )
 }
