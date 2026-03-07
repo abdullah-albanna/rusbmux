fbefdce9adf6f6f5f64eaed7473cd11b3e7e436d
Abdullah Al-Banna <abdu.albanna@proton.me>
2026-03-06 20:12:41 -0800

    send currently connected devices once a client is listening
    tell the new listeners about already connected devices
    
    this helps with macos finder

--- src/handler/listen.rs
+++ src/handler/listen.rs
@@ -1,8 +1,9 @@
 use crate::{
     AsyncWriting,
-    device::{DeviceEvent, HOTPLUG_EVENT_TX},
+    device::{CONNECTED_DEVICES, DeviceEvent, HOTPLUG_EVENT_TX},
     handler::device_list::create_device_connected_plist,
     parser::usbmux::{UsbMuxMsgType, UsbMuxPacket, UsbMuxVersion},
+    utils::nusb_speed_to_number,
 };
 
 use tokio::io::AsyncWriteExt;
@@ -12,6 +13,37 @@ pub async fn handle_listen(writer: &mut impl AsyncWriting, tag: u32) {
         .get()
         .expect("the device watcher hasen't been initlized (very unlikely)")
         .subscribe();
+
+    for device in CONNECTED_DEVICES.read().await.iter() {
+        #[cfg(any(target_os = "linux", target_os = "android"))]
+        let location_id = (device.info.busnum() as u32) << 16 | device.info.device_address() as u32;
+
+        #[cfg(target_os = "macos")]
+        let location_id = device.info.location_id();
+
+        let device_plist = create_device_connected_plist(
+            device.id,
+            nusb_speed_to_number(device.info.speed().unwrap_or(nusb::Speed::Low)),
+            location_id,
+            device.info.product_id(),
+            device.info.serial_number().unwrap_or_default().to_string(),
+        );
+
+        let device_xml = plist_macro::plist_value_to_xml_bytes(&device_plist);
+
+        let connected_packet = UsbMuxPacket::encode_from(
+            device_xml,
+            UsbMuxVersion::Plist,
+            UsbMuxMsgType::MessagePlist,
+            tag,
+        );
+
+        if let Err(_e) = writer.write_all(&connected_packet).await {
+            // eprintln!("unable to send the listen connect event, {e}");
+        }
+
+        writer.flush().await.unwrap();
+    }
 
     while let Ok(event) = event_receiver.recv().await {
         match event {
