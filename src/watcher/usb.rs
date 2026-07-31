9f7b1e9ac2d483bcecb9f822044d810c4be74142
se2crid <151872490+se2crid@users.noreply.github.com>
2026-07-31 22:43:35 +0200

    feat: preflight

--- src/watcher/usb.rs
+++ src/watcher/usb.rs
@@ -110,7 +110,7 @@ pub async fn watch_usb_daemon(backend: impl UsbBackend) {
                         // stale entry before registering the new connection.
                         CONNECTED_DEVICES.retain(|_, d| d.serial_number() != device.serial_number());
 
-                        // TODO: do preflight
+                        super::preflight::spawn(device.as_usb().unwrap().clone());
                         CONNECTED_DEVICES.insert(id, device);
                     }
                     Ok(UsbEvent::Disconnected(id)) => {
@@ -150,6 +150,7 @@ pub async fn watch_usb_daemon(backend: impl UsbBackend) {
                 {
                     device.as_usb().unwrap().set_disconnected_tx(disconnected_tx.clone());
 
+                    super::preflight::spawn(device.as_usb().unwrap().clone());
                     CONNECTED_DEVICES.insert(id, device);
 
                     let _ = hotplug_event_tx.send(DeviceEvent::Attached { id });
